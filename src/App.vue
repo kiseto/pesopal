99f177badba864e86f4873ca5cb5983f7ecd5de6
drake sekito <98731344+kiseto@users.noreply.github.com>
2025-09-22 17:14:33 +0800

    Vue initialized

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex flex-col justify-cente text-white">
+    <h1>Welcome to PesoPal</h1>
+    <p>We deliver great opportunities to the masses.</p>
+  </div>
+  
+  
 </template>
 
 <style scoped></style>
