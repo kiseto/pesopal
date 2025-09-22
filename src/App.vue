16a7cdb8b6fd9a5330b5abf12a37403b9e6c9775
drake sekito <98731344+kiseto@users.noreply.github.com>
2025-09-22 17:23:41 +0800

    Vue + Tailwind
    installed tailwind via vite

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
+
   <div class="flex flex-col justify-cente text-white">
     <h1>Welcome to PesoPal</h1>
-    <p>We deliver great opportunities to the masses.</p>
+    <p>We deliver great opportunities to the masses.</p>  
   </div>
-  
   
 </template>
 
